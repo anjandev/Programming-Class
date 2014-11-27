c3a54cf2c0aa4962c3713553a3ff5a2233a58934
Anjan Momi <anjan@momi.ca>
2014-11-27 13:04:03 -0800

    Added functions

--- Solitaire.py
+++ Solitaire.py
@@ -3,15 +3,6 @@
 import simplegui
 import random
 
-
-def assign_loc(numofcards, layer):
-    global shuffled
-    
-    for x in range(numofcards):
-        layer.append(shuffled[x])
-        shuffled[x] = Card(layer, suit, rank, "no")
-        shuffled.pop(0)
-        
 # Classes
 class Card:
 
@@ -22,12 +13,13 @@ class Card:
         self.exposed = exposed
     
     def __str__(self):
-        return self.value + self.suit
+        return self.value + " of " + self.suit
         
 # Global variables
 ranks = ("King", "Queen", "Joker", "10", "9", "8", "7", "6", "5", "4", "3", "2", "Ace")
-suits = (" of Diamonds", " of Spades", " of Hearts", " of Clovers")
+suits = ("Diamonds", "Spades", "Hearts", "Clovers")
 deck = []
+shuffled = []
 
 layer1 = []
 layer2 = []
@@ -42,41 +34,49 @@ top2 = []
 top3 = []
 top4 = []
 
-for rank in ranks:
-    for suit in suits:
-        deck.append(rank + suit)
-        
-shuffled = deck
-random.shuffle(shuffled)
-
-
-for card in shuffled:
-    card = Card(shuffled, suit, rank, "no")
-    
-print shuffled
-
-assign_loc(1, layer1)
-assign_loc(2, layer2)
-assign_loc(3, layer3)
-assign_loc(4, layer4)
-assign_loc(5, layer5)
-assign_loc(6, layer6)
-assign_loc(7, layer7)
-
-
-print layer1
-print layer2
-print layer3
-print layer4
-
-
-    
-
 
 # Helper Functions
+def main():
+    global deck, shuffled
+    
+    makedeck()
+    shuffle()
+    assign_loc(1, layer1)
+    assign_loc(2, layer2)
+    assign_loc(3, layer3)
+    assign_loc(4, layer4)
+    assign_loc(5, layer5)
+    assign_loc(6, layer6)
+    assign_loc(7, layer7)
+    
+    
+def assign_loc(numofcards, layer):
+    global shuffled
+    
+    for x in range(numofcards):
+        layer.append(shuffled[x])
+        # To-do change location of card that was appended
+        shuffled.pop(0)
 
+def makedeck():
+    global ranks, suits, deck, shuffled
+    
+    for rank in ranks:
+        for suit in suits:
+            print rank + suit
+            deck.append(rank + suit)
+            # To-do Assign card images
+            deck[-1] = Card("shuffled", suit, rank, "no")
 
-
+def shuffle():
+    global deck, shuffled
+    
+    for card in deck:
+        shuffled.append(card)
+        
+    random.shuffle(shuffled)
+    
+    
 # Event Handlers
 
 # Make Frame
@@ -84,3 +84,5 @@ print layer4
 # Call Event Handlers
 
 # Start Frame and Timers
+
+main()
