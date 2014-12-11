95d616ccd677454b7e62aade2673164733f057bf
Anjan Momi <anjan@momi.ca>
2014-12-11 11:17:21 -0800

    layer movement is semi-working

--- Solitaire/Shuffling_animation.py
+++ Solitaire/Shuffling_animation.py
@@ -1,78 +1,90 @@
-# A cool shuffling animation I made while trying to get the deck to work. Might get this to play
-# before the game begins.
+# Solitaire Game by Anjan Momi
 
 import simplegui
 import random
 
 # Link to sets http://imgur.com/dEoEdBG,rjCP3uF,HfL7T9o,B0PojjI
-# Current bugs. Layer 2 draw repeats 4 times
-# Layer 3 draw repeats 9
-# My card width probably go off the end of the set.
-# Missing Cards: 6 of spades
-# 8 of spades
-# 2 of spades
-# Queen of spades
-# 5 of spades
 
 # Classes
 class Card:
 
-    def __init__(self, location, suit, value, exposed, exposedimg, cardnum):
+    def __init__(self, location, suit, value, exposed, exposedimg, cardnum, colour, LOC_BACK_X, LOC_BACK_Y):
         self.location = location
         self.suit = suit
         self.value = value
         self.exposed = exposed
         self.exposedimg = exposedimg
         self.cardnum = cardnum
-
+        self.LOC_BACK_X = LOC_BACK_X
+        self.LOC_BACK_Y = LOC_BACK_Y
+        self.colour = colour
+    
     def __str__(self):
-        return self.value + " of " + self.suit + ". It's location is" + self.location + ". Exposed:" + str(self.exposed) + ". Exposed image is:" + str(self.exposedimg) + " Cardnum:" + str(self.cardnum)
-
+        return str(self.suit) + " " + str(self.value)
+    
     # Getters
     def get_exposed(self):
         return self.exposed
-
+    
     def get_exposedimg(self):
         return self.exposedimg
-
+    
     def get_cardnum(self):
         return self.cardnum
-
-    def get_cardfront(self, canvas, side, LOC_BACK_X, LOC_BACK_Y):
-
+    
+    def get_LOC_BACK_X(self):
+        return self.LOC_BACK_X
+    
+    def get_LOC_BACK_Y(self):
+        return self.LOC_BACK_Y
+    
+    def get_colour(self):
+        return self.colour
+    
+    def get_location(self):
+        return self.location
+    
+    def get_cardfront(self, canvas):
+        
         CARDWIDTH = 167
         CARDHEIGHT = 243
-        CARD_SPOT_IN_SETIMG = self.cardnum
         DRAWSCALE = 0.5
-
-        if side == "front":
-            SET = self.exposedimg
-            # TODO make this work if back is selected (change pic to back)
-        elif side == "back":
-            SET = simplegui.load_image('http://i.imgur.com/p6hCw9U.png')
-            CARDWIDTH = SET.get_width()
-            CARDHEIGHT = SET.get_height()
-            DRAWSCALE = 1.2
-
-        return canvas.draw_image(SET, (CARDWIDTH / 2 + CARDWIDTH * CARD_SPOT_IN_SETIMG, CARDHEIGHT / 2),
-                     (CARDWIDTH, CARDHEIGHT),
-                     (LOC_BACK_X, LOC_BACK_Y),
+        
+        CARD_SPOT_IN_SETIMG = self.cardnum
+        LOC_BACK_X = self.LOC_BACK_X
+        LOC_BACK_Y = self.LOC_BACK_Y
+        
+        SET = self.exposedimg
+            
+        return canvas.draw_image(SET, (CARDWIDTH / 2 + CARDWIDTH * CARD_SPOT_IN_SETIMG, CARDHEIGHT / 2), 
+                     (CARDWIDTH, CARDHEIGHT), 
+                     (LOC_BACK_X, LOC_BACK_Y), 
                      (CARDWIDTH * DRAWSCALE, CARDHEIGHT * DRAWSCALE))
-
+    
     # Setters
-
+    
     def set_location(self, location):
         self.location = location
-
+        
     def set_exposed(self, exposed):
         self.exposed = exposed
+    
+    def set_X(self, LOC_BACK_X):
+        self.LOC_BACK_X = LOC_BACK_X
+        
+    def set_Y(self, LOC_BACK_Y):
+        self.LOC_BACK_Y = LOC_BACK_Y
 
-
+   
 # Global variables
+layer_dic = {'red': 'black', 'black': 'red'}
+
 ranks = ("Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Joker", "Queen", "King")
 suits = ("Diamonds", "Spades", "Hearts", "Clovers")
 deck = []
 shuffled = []
+
+cards_shown = []
 
 layer1 = []
 layer2 = []
@@ -87,15 +99,28 @@ top2 = []
 top3 = []
 top4 = []
 
-location_in_deck = 0
+location_in_deck = 0 
 
-x1 = 0
-x2 = 0
-y1 = 0
-y2 = 0
-clicknum = 0
+x = 0
+y = 0
+clickednum = 0
+clickedlayer1 = " "
+clickedlayer2 = " "
 
-# How many times the player has clicked the deck. So that
+deckcard1 = 0
+deckcard2 = 0
+deckcard3 = 0
+
+layercard1 = 0
+layercard2 = 0
+layercard3 = 0
+layercard4 = 0
+layercard5 = 0
+layercard6 = 0
+layercard7 = 0
+
+
+# How many times the player has clicked the deck. So that 
 # the same 3 cards everytime together when they click the
 # deck unless they move something out the deck.
 timesdecked = 0
@@ -106,166 +131,252 @@ heartsimg = simplegui.load_image("http://i.imgur.com/HfL7T9o.png")
 cloversimg = simplegui.load_image("http://i.imgur.com/dEoEdBG.png")
 
 
-# Where the rank starts in the deck. Made changes. May cause bugs.
-
 # Helper Functions
 def main():
     global deck, shuffled, diamonds, spades, hearts, clovers
     global layer1, layer2, layer3, layer4, layer5, layer6
     global layer7, spadesimg, diamondsimg, heartsimg, cloversimg
-
+    
     makedeck()
-
+    
     ranklen = 13
     diamondsstart = 0 * ranklen
     spadesstart = 1 * ranklen
     heartsstart = 2 * ranklen
     cloversstart = 3 * ranklen
 
-
-    definecards(diamondsstart, 'Diamonds', diamondsimg)
-    definecards(spadesstart, 'Spades', spadesimg)
-    definecards(heartsstart, 'Hearts', heartsimg)
-    definecards(cloversstart, 'Clovers', cloversimg)
-
+    definecards(diamondsstart, 'Diamonds', diamondsimg, "red")
+    definecards(spadesstart, 'Spades', spadesimg, "black")
+    definecards(heartsstart, 'Hearts', heartsimg, "red")
+    definecards(cloversstart, 'Clovers', cloversimg, "black")
+    
     shuffle()
-
+    
     assign_loc(1, layer1, "layer1")
     layer1[-1].set_exposed(True)
+
     assign_loc(2, layer2, "layer2")
     layer2[-1].set_exposed(True)
+
     assign_loc(3, layer3, "layer3")
     layer3[-1].set_exposed(True)
+
     assign_loc(4, layer4, "layer4")
     layer4[-1].set_exposed(True)
+
     assign_loc(5, layer5, "layer5")
     layer5[-1].set_exposed(True)
+
     assign_loc(6, layer6, "layer6")
     layer6[-1].set_exposed(True)
+    
     assign_loc(7, layer7, "layer7")
     layer7[-1].set_exposed(True)
-
-    print deck[0]
-
-
+    
+    
 def assign_loc(numofcards, layer, name_of_layer):
     global shuffled, Cards
-
+    
     for x in range(numofcards):
         layer.append(shuffled[0])
         # To-do change location of card that was appended
         shuffled[0].set_location(name_of_layer)
         shuffled.pop(0)
 
-
+      
 def makedeck():
     global ranks, suits, deck, shuffled
-
+    
     for suit in suits:
         for rank in ranks:
             deck.append(rank + suit)
-
-
-def definecards(deckstart, suit, setimg):
+           
+            
+def definecards(deckstart, suit, setimg, colour):
     global deck, ranks
-
-    print suit
-
+   
     for x in range(13):
-        print deck[x + deckstart]
-        print [x + deckstart]
-        deck[x + deckstart] = Card("shuffled", suit, ranks[x], False, setimg, x)
-
-
+        deck[x + deckstart] = Card("shuffled", suit, ranks[x], False, setimg, x, colour, 0, 0)
+    
+        
 def shuffle():
     global deck, shuffled
-
+    
     for card in deck:
         shuffled.append(card)
-
     random.shuffle(shuffled)
 
-
+    
 def drawlayer(layer, layernum, canvas):
-
-    for cards in range(len(layer)):
-        BACK = simplegui.load_image('http://i.imgur.com/p6hCw9U.png')
-        BACKWIDTH = BACK.get_width()
-        BACKHEIGHT = BACK.get_height()
-        LOC_BACK_Y = 300
-        LOC_BACK_X = 100 * layernum
+    
+    for card in range(len(layer)):
+        
         DRAWSCALE = 1.25
         DIST_FROM_LAST = 25
-        LOC_BACK_Y = LOC_BACK_Y + DIST_FROM_LAST * cards
-
-        if layer[cards].exposed == False:
-            # Because the back for all cards is the same, I can use any
-            # card from my list of cards(the deck) to draw the back.
-            # Thats why I chose deck[0]. It's completely abritrary.
-            deck[0].get_cardfront(canvas, "back", LOC_BACK_X, LOC_BACK_Y)
-
+        
+        LOC_BACK_Y = 300
+        
+        LOC_BACK_Y = LOC_BACK_Y + DIST_FROM_LAST * card
+        LOC_BACK_X = 100 * layernum
+        
+        if layer[card].exposed == False:
+            draw_card_back(canvas, LOC_BACK_X, LOC_BACK_Y)
+            
         else:
-            # Width of one card. This might create a bug
             CARDWIDTH = 167
             CARDHEIGHT = 243
-            SET = layer[cards].exposedimg
-            CARD_SPOT_IN_SETIMG = layer[cards].cardnum
+            
+            layer[card].set_X(LOC_BACK_X)
+            layer[card].set_Y(LOC_BACK_Y)
+            
+            SET = layer[card].exposedimg
+            CARD_SPOT_IN_SETIMG = layer[card].cardnum
+            layer[card].get_cardfront(canvas)
 
-            layer[cards].get_cardfront(canvas, "front", LOC_BACK_X, LOC_BACK_Y)
-
-
+            
 def draw_new_set_from_deck(canvas):
-    global shuffled, location_in_deck, x1, x2, y1, y2
+    global cards_shown
+    
+    LOC_BACK_X = 100
+    LOC_BACK_Y = 75
+   
+    for card in range(len(cards_shown)):
+        LOC_BACK_X += 85
+        cards_shown[card].get_cardfront(canvas)
+        cards_shown[card].set_X(LOC_BACK_X)
+        cards_shown[card].set_Y(LOC_BACK_Y)
 
+        
+def click_new_set_from_deck():
+    global x, y, location_in_deck, cards_shown
+    
     TOPLEFTDECK_Y = 19
     TOPLEFTDECK_X = 59
-
+    
     BOTTOMRIGHTDECK_Y = 131
     BOTTOMRIGHTDECK_X = 143
-
+    
     LOC_BACK_Y = 75
-    LOC_BACK_X = 100
-
-    x = x2
-    y = y2
-
+    LOC_BACK_X = 100 
+    
+        
     # Check if player has clicked on the deck
     if BOTTOMRIGHTDECK_X > x > TOPLEFTDECK_X:
         if BOTTOMRIGHTDECK_Y > y > TOPLEFTDECK_Y:
-            print "clicking m8"
+            
             location_in_deck += 3
-
+            
             cards_shown = []
-
+            
             num_of_cards = len(shuffled)
-
+            
             if location_in_deck == num_of_cards:
                 location_in_deck = 0
+                cards_shown = []
+                
             else:
+                for cards in cards_shown:
+                    cards_shown[card].set_X("deck")
+                    cards_shown[card].set_Y("deck")
+                
+                cards_shown = []
+                # Add if else statement here when the feature to move cards
+                # out of the deck is added. Potential bug
                 for x in range(3):
                     cards_shown.append(shuffled[x + location_in_deck])
-
-                for card in range(3):
-                    LOC_BACK_X += 85
-                    cards_shown[card].get_cardfront(canvas, "front", LOC_BACK_X, LOC_BACK_Y)
-
+                    cards_shown[x].set_exposed(True)
         else:
             pass
     else:
+        pass 
+    
+    
+def draw_card_back(canvas, LOC_BACK_X, LOC_BACK_Y):
+    
+    SET = simplegui.load_image('http://i.imgur.com/p6hCw9U.png')
+    CARDWIDTH = SET.get_width()
+    CARDHEIGHT = SET.get_height()
+    DRAWSCALE = 1.2
+            
+    return canvas.draw_image(SET, (CARDWIDTH /2 , CARDHEIGHT / 2), 
+                     (CARDWIDTH, CARDHEIGHT), 
+                     (LOC_BACK_X, LOC_BACK_Y), 
+                     (CARDWIDTH * DRAWSCALE, CARDHEIGHT * DRAWSCALE))
+
+
+def check_if_click_on_layer(layer, layernum):
+    global y, x, clickedlayer1, clickednum, clickedlayer2
+    
+    CARDWIDTH = 85 / 2
+    CARDHEIGHT = 125 / 2
+    
+    if len(layer) > 0:
+        DIST_FROM_LAST = 25
+        LOC_BACK_Y = 277
+        
+        LOC_BACK_Y = LOC_BACK_Y + DIST_FROM_LAST * len(layer)
+        LOC_BACK_X = 100 * layernum
+        
+        topleft_X = LOC_BACK_X - CARDWIDTH
+        topleft_Y = LOC_BACK_Y - CARDHEIGHT
+        
+        bottomright_X = LOC_BACK_X + CARDWIDTH
+        bottomright_Y = LOC_BACK_Y + CARDHEIGHT
+        
+        if bottomright_X > x > topleft_X:
+            if bottomright_Y > y > topleft_Y:
+                if clickednum == 0:
+                    clickedlayer1 = layer
+                    clickednum = 1
+                else:
+                    clickedlayer2 = layer
+                    clickednum = 0  
+            else:
+                pass
+        else: 
+            pass
+    
+    else: 
         pass
+    
+def check_if_colours_match():
+    global clickedlayer1, clickedlayer2, layer_dic
+    global layer1, layer2, layer3, layer4, layer5, layer6,
+    global layer7
 
+    if clickedlayer2 == " ":
+        pass
+    
+    else:
+        card1colour = clickedlayer1[-1].get_colour()
+        card2colour = clickedlayer2[-1].get_colour()
+        
+        if layer_dic[card1colour] == card2colour:
+            print clickedlayer1
+            print clickedlayer1[-1]
+            print clickedlayer2[-1]
+            clickedlayer1.append(clickedlayer2[-1])
+            clickedlayer1.pop()
+            print clickedlayer2
+            print clickedlayer1
+        elif layer_dic[card2colour] == card1colour:
+            pass
+        else:
+            pass
+        
 
+    
 # Event Handlers
 
 def draw_handler(canvas):
-    global layer1, layer2, layer3, layer4, layer5, layer6, layer7
-    global DRAWSCALE, deck
-
+    global layer1, layer2, layer3, layer4, layer5, layer6, layer7 
+    global DRAWSCALE, deck, cards_shown
+    
     # Drawing Deck
     LOC_BACK_Y = 75
     LOC_BACK_X = 100
-    deck[0].get_cardfront(canvas, "back", LOC_BACK_X, LOC_BACK_Y)
-
+    draw_card_back(canvas, LOC_BACK_X, LOC_BACK_Y)
+    
     drawlayer(layer1, 1, canvas)
     drawlayer(layer2, 2, canvas)
     drawlayer(layer3, 3, canvas)
@@ -273,34 +384,28 @@ def draw_handler(canvas):
     drawlayer(layer5, 5, canvas)
     drawlayer(layer6, 6, canvas)
     drawlayer(layer7, 7, canvas)
-
+    
     draw_new_set_from_deck(canvas)
-
-
+    
+    
 def mouse_handler(position):
-    global x1, x2, y1, y2, clicknum
-
-    # To play need to select a card and then click on what you
-    # where you want to put it. This allows us to keep track
-    # of what was selected (x1/y1) and where it was put(x2/y2).
-
-    print clicknum
-    print x1,
-    print y1
-
-    print x2,
-    print y2
-
-    if clicknum == 0:
-        x1 = position[0]
-        y1 = position[1]
-        clicknum = 1
-    elif clicknum == 1:
-        x2 = position[0]
-        y2 = position[1]
-        clicknum = 0
-
-
+    global x, y, layer1, layer2, layer3, layer4, layer5, layer6, layer7
+    
+    x = position[0]
+    y = position[1]
+    
+    click_new_set_from_deck()
+    
+    check_if_click_on_layer(layer1, 1)
+    check_if_click_on_layer(layer2, 2)
+    check_if_click_on_layer(layer3, 3)
+    check_if_click_on_layer(layer4, 4)
+    check_if_click_on_layer(layer5, 5)
+    check_if_click_on_layer(layer6, 6)
+    check_if_click_on_layer(layer7, 7)
+    
+    check_if_colours_match()
+    
 # Make Frame
 frame = simplegui.create_frame('Solitaire', 1000, 850)
 frame.set_canvas_background('Green')
