98f776c2c3369e352fa9b953bac78deb8c4059fc
Anjan Momi <anjan@momi.ca>
2015-01-16 13:02:51 -0800

    Create Problem S2.py

---
+++ Competition Practice/Problem S2.py
@@ -0,0 +1,8 @@
+# Problem S2: Assigning Partners 
+# Senior Division
+
+
+numberofkids = int(input("Enter the number of kids in the class"))
+
+names = input("Enter the number of kids in the class")
+
